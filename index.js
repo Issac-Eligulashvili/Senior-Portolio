ba0ca63d5bfb4e8dfa29a684b0d85c6b9fc0ccc8
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-03-31 14:57:58 -0400

    added the name array:
    TODO:
    
    fix the rotate so that its rotateX and fix it so that odd indicies have the transform multiplied by -1

--- index.js
+++ index.js
@@ -5,8 +5,31 @@ const app = Vue.createApp({
           }
      },
      methods: {
-          
+          updateElements() {
+               this.$forceUpdate(); // Forces Vue to recompute the computed property
+          }
      },
+     mounted() {
+          window.addEventListener("resize", this.updateElements);
+     },
+     beforeUnmount() {
+          window.removeEventListener("resize", this.updateElements);
+     },
+     computed: {
+          nameArray() {
+               const maxHeight = window.innerHeight - 208;
+               const count = Math.floor(maxHeight / 137);
+               const arr = [];
+               for (let i = 0; i < count; i++) {
+                    arr.push("ISSAC ELIGULASHVILI");
+               }
+               return arr;
+          },
+          containerHeight() {
+               const regHeight = this.nameArray.length * 192;
+               return regHeight - 30*this.nameArray.length;
+          }
+     }
 }).mount("#app");
 
 
