d71f91545f21d4ffce8c787d698cdc7f2640ade6
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-04-04 11:56:20 -0400

    added second section

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 const app = Vue.createApp({
      data() {
           return {
-               
+               projects: [
+                    {image: null, link: ""},
+                    {image: null, link: ""},
+                    {image: null, link: ""}
+               ]
           }
      },
      methods: {
           updateElements() {
                this.$forceUpdate(); // Forces Vue to recompute the computed property
           },
-          calculateStyle(index) {
-                
-          }
+
      },
      mounted() {
           window.addEventListener("resize", this.updateElements);
@@ -44,3 +46,32 @@ document.addEventListener("mousemove", (e) =>{
           left: `${e.clientX - (blob.offsetWidth / 2)}px`
      }, {duration: 1000, fill: "forwards"})
 })
+
+  let sections = document.querySelectorAll(".section");
+  let isScrolling = false;
+
+  window.addEventListener("wheel", (e) => {
+    if (isScrolling) return;
+
+    isScrolling = true;
+    const direction = e.deltaY > 0 ? 1 : -1;
+
+    let scrollPos = window.scrollY;
+    let currentIndex = Array.from(sections).findIndex(
+      (section) => section.offsetTop === scrollPos
+    );
+
+    let nextIndex = Math.min(
+      sections.length - 1,
+      Math.max(0, currentIndex + direction)
+    );
+
+    window.scrollTo({
+      top: sections[nextIndex].offsetTop,
+      behavior: "smooth",
+    });
+
+    setTimeout(() => {
+      isScrolling = false;
+    }, 1000); // adjust duration here (in ms)
+  });
