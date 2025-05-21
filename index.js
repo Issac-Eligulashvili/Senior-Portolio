37b0e84551d8ecd0a81923b43b684d38ebef1561
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-05-21 14:56:21 -0400

    working on adding the gallery

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const app = Vue.createApp({
      data() {
           return {
                projects: [
-                    {image: null, link: ""},
-                    {image: null, link: ""},
+                    {image: "img/wdpp.png", link: ""},
+                    {image: "img/sellmycar.png", link: ""},
                     {image: null, link: ""}
                ]
           }
@@ -39,39 +39,4 @@ const app = Vue.createApp({
 
 
 
-document.addEventListener("mousemove", (e) =>{
-     const blob = document.getElementById("feintCursorFollow");
-     blob.animate({
-          top: `${e.clientY - (blob.offsetHeight / 2)}px`,
-          left: `${e.clientX - (blob.offsetWidth / 2)}px`
-     }, {duration: 375, fill: "forwards"})
-})
-
-  let sections = document.querySelectorAll(".section");
-  let isScrolling = false;
-
-  window.addEventListener("wheel", (e) => {
-    if (isScrolling) return;
-
-    isScrolling = true;
-    const direction = e.deltaY > 0 ? 1 : -1;
-
-    let scrollPos = window.scrollY;
-    let currentIndex = Array.from(sections).findIndex(
-      (section) => section.offsetTop === scrollPos
-    );
-
-    let nextIndex = Math.min(
-      sections.length - 1,
-      Math.max(0, currentIndex + direction)
-    );
-
-    window.scrollTo({
-      top: sections[nextIndex].offsetTop,
-      behavior: "smooth",
-    });
-
-    setTimeout(() => {
-      isScrolling = false;
-    }, 1000); // adjust duration here (in ms)
-  });
+     
