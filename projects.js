64dcdefd0b7c0af95c19ab159449a56fe46d4dc8
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-05-22 11:55:40 -0400

    finished the nav and now working it on it growing to the full screen

--- projects.js
+++ projects.js
@@ -1,10 +1,28 @@
-setTimeout(() => {
-     let configurationArray = ['config1', 'config2', 'config3'];
-     let previousInt = 0;
+let previousInt = 0;
+let hovering = false;
+let active = false;
+setInterval(() => {
+     if (!hovering && !active) {
+          let configurationArray = ['config1', 'config2', 'config3'];
      let randInt = Math.floor(Math.random() * 3);
      while(randInt === previousInt) {
           randInt = Math.floor(Math.random() * 3);
      }
+     previousInt = randInt
 
      $("#wrapper").removeClass().addClass(configurationArray[randInt])
-}, 3000);
+     }
+}, 2000);
+
+$(document).ready(function () {
+     $("#wrapper").on("mouseenter", () => {
+          hovering = true;
+     })
+     $("#wrapper").on("mouseleave", () => {
+          hovering = false;
+     })
+     $(".shape").on("click", (event) => {
+          $(event.target).toggleClass("active");
+          active = !active;
+     })
+});
