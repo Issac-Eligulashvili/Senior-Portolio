1ea1ff14485566aaaacc4021a2d1c0641154c076
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-05-23 23:21:56 -0400

    finished the animation and made it cleannnnnn

--- projects.js
+++ projects.js
@@ -22,7 +22,28 @@ $(document).ready(function () {
           hovering = false;
      })
      $(".shape").on("click", (event) => {
-          $(event.target).toggleClass("active");
+          const el = $(event.target)
+     
+          el.addClass("expanding")
+          setTimeout(() => {
+               $(".shape").toggleClass("inactive");
+               if (el.data("moved")) {
+                    el.css("transform",`translate(0,0)`);
+                    el.data("moved", false)
+               } else {
+                    const offset = el.offset();
+                    console.log(offset);
+                    const dx = -offset.left;
+                    const dy = -offset.top;
+                    console.log(dy, dx);
+                    el.css("transform", `translate(${dx}px, ${dy}px)`)
+                    el.data("moved", true);
+               }
+
+
+
+               el.removeClass("inactive").toggleClass("active");
+          }, 0)
           active = !active;
      })
 });
