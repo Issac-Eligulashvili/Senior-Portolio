b1d8d0091f8958a11c6a1b1e219912e0c8cd31eb
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-05-24 18:05:59 -0400

    working on developing the pages for the websites for the gallery

--- projects.js
+++ projects.js
@@ -1,49 +1,71 @@
-let previousInt = 0;
-let hovering = false;
-let active = false;
-setInterval(() => {
-     if (!hovering && !active) {
-          let configurationArray = ['config1', 'config2', 'config3'];
-     let randInt = Math.floor(Math.random() * 3);
-     while(randInt === previousInt) {
-          randInt = Math.floor(Math.random() * 3);
-     }
-     previousInt = randInt
-
-     $("#wrapper").removeClass().addClass(configurationArray[randInt])
-     }
-}, 2000);
-
 $(document).ready(function () {
+     let previousInt = 0;
+     let hovering = false;
+     let active = false;
+     let currentSlideIndex = 0;
+     setInterval(() => {
+          if (!hovering && !active) {
+               let configurationArray = ['config1', 'config2', 'config3'];
+          let randInt = Math.floor(Math.random() * 3);
+          while(randInt === previousInt) {
+               randInt = Math.floor(Math.random() * 3);
+          }
+          previousInt = randInt
+     
+          $("#wrapper").removeClass().addClass(configurationArray[randInt])
+          }
+     }, 2000);
      $("#wrapper").on("mouseenter", () => {
           hovering = true;
      })
      $("#wrapper").on("mouseleave", () => {
           hovering = false;
      })
-     $(".shape").on("click", (event) => {
-          const el = $(event.target)
-     
-          el.addClass("expanding")
-          setTimeout(() => {
-               $(".shape").toggleClass("inactive");
-               if (el.data("moved")) {
-                    el.css("transform",`translate(0,0)`);
-                    el.data("moved", false)
-               } else {
-                    const offset = el.offset();
-                    console.log(offset);
-                    const dx = -offset.left;
-                    const dy = -offset.top;
-                    console.log(dy, dx);
-                    el.css("transform", `translate(${dx}px, ${dy}px)`)
-                    el.data("moved", true);
-               }
-
-
-
-               el.removeClass("inactive").toggleClass("active");
-          }, 0)
-          active = !active;
+     $(".shape").on("click", function() {
+          console.log("clicked");
+          const el = $(this)
+          if (!el.hasClass("active")) {
+               $(".shape").addClass("inactive");
+               const offset = el.offset();
+               const dx = -offset.left;
+               const dy = -offset.top;
+               $(".navContainer").addClass("hide");
+               el.css("transform", `translate(${dx}px, ${dy}px)`)
+               el.data("moved", true);
+               el.removeClass("inactive").addClass("active");
+               active = !active;
+          }
      })
+     $(".goBackBtn").on("click", function(event) {
+          event.stopPropagation();
+          console.log(currentSlideIndex);
+          if (currentSlideIndex <= 0) {
+               const el = $(this).parents().eq(0);
+               $(".shape").removeClass("inactive");
+               el.css("transform",`translate(0,0)`);
+               $(".navContainer").removeClass("hide");
+               el.data("moved", false)
+               el.removeClass("active");
+               active = !active;
+          } else {
+               const el = $(this).nextAll().eq(1);
+               const prev = el.children(currentSlideIndex - 1);
+               const transform = el.css("transform");
+               const values = transform.match(/matrix\(([^)]+)\)/)[1].split(', ');
+               const offset = prev.offset();
+               const dx = -values[4] + offset.left;
+               el.css("transform", `translate(${dx}px, 0px)`);
+               currentSlideIndex--;
+          }
+     });
+     $(".goFwdBtn").on("click", function(event) {
+          event.stopPropagation();
+          const el = $(this).next().eq(0);
+          const next = el.children().eq(currentSlideIndex + 1);
+          const offset = next.offset();
+          const dx = -offset.left
+          el.css("transform", `translate(${dx}px, 0px)`);
+          currentSlideIndex++;
+     })
+
 });
