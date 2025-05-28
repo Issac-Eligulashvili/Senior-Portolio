7619870026235f5c598886f543de98bd2a4821c6
Issac-Eligulashvili <issac.eli2007@gmail.com>
2025-05-28 14:52:31 -0400

    added GSAP and finished the home animations

--- index.js
+++ index.js
@@ -12,11 +12,27 @@ const app = Vue.createApp({
           updateElements() {
                this.$forceUpdate(); // Forces Vue to recompute the computed property
           },
-
      },
      mounted() {
           window.addEventListener("resize", this.updateElements);
-     },
+
+          const gsap = window.gsap;
+          const ScrollTrigger = window.ScrollTrigger;
+
+          gsap.registerPlugin(ScrollTrigger);
+          gsap.utils.toArray([".delay-1", ".delay-2"]).forEach(el => {
+               gsap.to(el, {
+                         x: "-100%",
+                         scrollTrigger: {
+                         trigger: ".names-container",
+                         start: "top top",
+                         end: "bottom top",
+                         scrub: true,
+
+                    }
+               });
+          })
+     },    
      beforeUnmount() {
           window.removeEventListener("resize", this.updateElements);
      },
@@ -37,6 +53,3 @@ const app = Vue.createApp({
      }
 }).mount("#app");
 
-
-
-     
